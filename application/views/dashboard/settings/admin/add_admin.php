cade3f7b6d869ae2471fe203662462656ab77cc5
Hemendra Kumar Sahu <me@bradtraversy.com>
2020-08-22 22:24:43 +0530

    minor correction in markup of the page

--- application/views/dashboard/settings/admin/add_admin.php
+++ application/views/dashboard/settings/admin/add_admin.php
@@ -1,8 +1,8 @@
-<!--Continuing from views/dashboard/templated/header.php-->
-<div class="text">
 <?php 	
 defined('BASEPATH') OR exit('No direct script access allowed');
- ?>	
+ ?><!--Continuing from views/dashboard/templated/header.php-->
+<div class="text">
+
 </div>
 		<div class="container dashborad-working-area">
 			<?php if(isset($execute)):?>
